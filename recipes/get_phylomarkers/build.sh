d9ba952533f4850d51b14bd97b2cfdf11a521783
eead-csic-compbio <bcontreras@eead.csic.es>
2024-03-27 13:21:53 +0100

    1st draft, paup & kdetrees still need to be taken care of

---
+++ recipes/get_phylomarkers/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# copy scripts and its dependencies to $PREFIX/bin folder,
+# note that db/ does not contain swissprot nor Pfam data
+# to keep the package small
+
+mkdir -p $PREFIX/bin
+cp -ar lib \
+    bin \
+    test_sequences \
+    pop_gen_tables \
+    *.pl \
+    *.sh \
+    *-R \
+    CHANGES.txt \
+    LICENSE.txt \
+    $PREFIX/bin
